a788e0e257dd408afe4607d638ae128341bbe23c
muhammadali-husanov <90840950+muhammadali-husanov@users.noreply.github.com>
2021-09-16 16:48:48 +0500

    FANSHINLAR

---
+++ index.js
@@ -0,0 +1,16 @@
+// var pl = [ "python" , "JavaScript","PHP","Rust" ]
+// for ( var i = 0; i< pl.length; i++ ){
+//     if (pl[i] =="JavaScript"){
+//         console.log( pl[i].toUpperCase())
+//     }
+//     else console.log( pl[i])
+// }
+//  var j = 0
+//  for ( var i = 20; i > 0; i--){
+//         console.log(  " " .repeat(i)+ "/"+"*" .repeat(j)+  "*" + "*".repeat(j)  + "\\" ," ".repeat(i)  )
+//     j++;
+   
+
+//  }
+
+
